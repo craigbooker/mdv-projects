023abdc398d323b72cb85912a23bf9a110462b16
Craig Booker <craig@craigbooker.com>
2011-10-18 18:20:11 -0500

    Rebuilt getData function

--- MIU/booker_craig_project4/js/main.js
+++ MIU/booker_craig_project4/js/main.js
@@ -754,7 +754,43 @@ function bluildPage(id) {
 }
 
 
+// -------   Start of: Get Data NEW --------------------------
+	function getData() {
+		toggleControls("on");
+		if(localStorage.length === 0) {
+			autoFillData();
+			alert("There is no data in local Storage so default was added.");
+		}
+		// Write data from local storage to the browser
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$('items').style.display = "block";
+		for(var i=0, len = localStorage.length; i < len; i++) {
+			var makeLi	= document.createElement("li");
+			var linksLi		=	document.createElement("li");
+			makeList.appendChild(makeLi);
+			var key		= localStorage.key[i];
+			var value		=	localStorage.getItem[key];
+			// Convert the string from local storage value back to an object by using JSON.parse
+			var obj 		=	JSON.parse(value);
+			var makeSubList	=	document.createElement('ul');
+			makeLi.appendChild(makeSubList);
+			for(var n in obj) {
+				var makeSubLi	=	document.createElement('li');
+				makeSubList.appendChild(makeSubLi);
+				var optSubText	=	obj[n][0]+" "+obj[n][1];
+				makeSubLi.innerHTML	=	optSubText;
+				makeSubList.appendChild(linksLi);
+			}
+			makeItemLinks(localStorage.key(i), linksLi);
+		}
+	}
 
+
+// -------   End of: Get Data NEW --------------------------
 
 
 
