2b13d67cdc55aa9a373fbdc19d7ecd63eeb24029
Craig Booker <craig@craigbooker.com>
2011-10-19 09:58:28 -0500

    JSON file data formatted and complete.

--- MIU/booker_craig_project4/js/json.js
+++ MIU/booker_craig_project4/js/json.js
@@ -1,29 +1,25 @@
 	var json = {
-	//end of ERRAND tasks
-			errand: {
-				//description: "All your errands waiting to be done.",
-				tasks: [
-					{
+		"task1":{
 						"context": ["Context:", "Errand" ],
 						"name": ["Task Name:", "Get Vacuum Bags" ],
 						"priority": ["Priority:", "5" ],
-						"favorite": ["Favorite:", ["Favorite:", "checked" ],
+						"favorite": ["Favorite:",  "checked" ],
 						"sDate": ["Start Date", "2011-05-11" ],
 						"eDate": ["End Date:", "2011-05-12" ],
 						"dDate": ["Due Date:", "2011-05-12" ],
 						"notes": ["Notes:", "Don't forget to get the model fj2 bags." ]
 					},
-					{
+		"task2":{
 						"context": ["Context:", "Errand" ],
 						"name": ["Task Name:","Buy Broom"],
 						"priority": ["Priority:","5"],
-						"favorite": ["Favorite:", ["Favorite:", "unchecked"],
+						"favorite": ["Favorite:", "unchecked"],
 						"sDate": ["Start Date", "2011-05-11"],
 						"eDate": ["End Date:", "2011-05-11"],
 						"dDate": ["Due Date:", "2011-05-11"],
 						"notes": ["Notes:", "Large broom for garage."]
 					},		
-					{
+	"task3": {
 						"context": ["Context:", "Errand" ],
 						"name": ["Task Name:", "Buy blades for mower"],
 						"priority": ["Priority:","5"],
@@ -33,17 +29,17 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", "Honda SRT Model 5"]
 					},		
-					{
+	"task4": {
 						"context": ["Context:", "Errand" ],
 						"name": ["Task Name:", "Buy pansies"],
 						"priority": ["Priority:","5"],
-						"favorite": ["Favorite:", ["Favorite:", "unchecked"],
+						"favorite": ["Favorite:", "unchecked"],
 						"sDate": ["Start Date", "2011-05-11"],
 						"eDate": ["End Date:", "2011-05-14"],
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", "For front flowerbed, purple and red."]
 					},
-					{
+	"task5": {
 						"context": ["Context:", "Errand" ],
 						"name": ["Task Name:","Buy Grass Seed"],
 						"priority": ["Priority:","3"],
@@ -52,14 +48,8 @@
 						"eDate": ["End Date:", "2011-05-14"],
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", "Get fescue for backyard."]
-					}
-				],
-			}, //end of ERRAND tasks
-//Start of HOME tasks
-			home: {
-				description: "All your things around the house waiting to be done.",
-				tasks: [
-					{
+					},
+	"task6": {
 						"context": ["Context:", "Home" ],
 						"name": ["Task Name:", "Vacuum House"],
 						"priority": ["Priority:","5"],
@@ -69,7 +59,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task7": {
 						"context": ["Context:", "Home" ],
 						"name": ["Task Name:","Sweep Garage"],
 						"priority": ["Priority:","5"],
@@ -79,7 +69,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},		
-					{
+	"task8": {
 						"context": ["Context:", "Home" ],
 						"name": ["Task Name:", "Mow Lawn"],
 						"priority": ["Priority:", "5"],
@@ -89,7 +79,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},		
-					{
+	"task9": {
 						"context": ["Context:", "Home" ],
 						"name": ["Task Name:", "Plant Flowers"],
 						"priority": ["Priority:", "5"],
@@ -99,69 +89,67 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task10": {
 						"context": ["Context:", "Home" ],
 						"name": ["Task Name:", "Plant Grass Seed"],
 						"priority": ["Priority:", "5"],
-						"favorite": "checked"],
+						"favorite": ["Favorite:", "checked"],
 						"sDate": ["Start Date", "2011-05-11"],
 						"eDate": ["End Date:", "2011-05-14"],
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task11": {
 						"context": ["Context:", "Office" ],
 						"name": ["Task Name:", "Order Office Supplies"],
 						"priority": ["Priority:", "5"],
-						"favorite": ["", "checked"],
+						"favorite": ["Favorite:",  "checked"],
 						"sDate": ["Start Date", "2011-05-11"],
 						"eDate": ["End Date:", "2011-05-14"],
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task12": {
 						"context": ["Context:", "Office" ],
 						"name": ["Task Name:", "Clean Desk"],
 						"priority": ["Priority:", "5"],
-						"favorite": ["", "checked"],
+						"favorite": ["Favorite:", "checked"],
 						"sDate": ["Start Date", "2011-05-11"],
 						"eDate": ["End Date:", "2011-05-14"],
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},		
-					{
+	"task13": {
 						"context": ["Context:", "Office" ],
 						"name": ["Task Name:", "Reorganize Filecabinet"],
 						"priority": ["Priority:", "5"],
-						"favorite": ["", "checked"],
+						"favorite": ["Favorite:", "checked"],
 						"sDate": ["Start Date", "2011-05-11"],
 						"eDate": ["End Date:", "2011-05-14"],
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
-
 					},		
-					{
+	"task14": {
 						"context": ["Context:", "Office" ],
 						"name": ["Task Name:", "Meeting with Mike"],
 						"priority": ["Priority:", "5"],
-						"favorite": ["", "checked"],
+						"favorite": ["Favorite:", "checked"],
 						"sDate": ["Start Date", "2011-05-11"],
 						"eDate": ["End Date:", "2011-05-14"],
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
-
 					},
-					{
+	"task15": {
 						"context": ["Context:", "Office" ],
 						"name": ["Task Name:", "Prepare Sales Presentation"],
 						"priority": ["Priority:", "5"],
-						"favorite": ["", "checked"],
+						"favorite": ["Favorite:", "checked"],
 						"sDate": ["Start Date", "2011-05-11"],
 						"eDate": ["End Date:", "2011-05-14"],
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task16": {
 						"context": ["Context:", "Calls" ],
 						"name": ["Task Name:","Call Joe from Shipping"],
 						"priority": ["Priority:","5"],
@@ -171,7 +159,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task17": {
 						"context": ["Context:", "Calls" ],
 						"name": ["Task Name:", "Call Car Dealer About Repair Status"],
 						"priority": ["Priority:","5"],
@@ -181,7 +169,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},		
-					{
+	"task18": {
 						"context": ["Context:", "Calls" ],
 						"name": ["Task Name:", "Call AT&T About Texting Plan"],
 						"priority": ["Priority:", "5"],
@@ -191,7 +179,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},		
-					{
+	"task19": {
 						"context": ["Context:", "Calls" ],
 						"name": ["Task Name:", "Call Client to Setup Meeting"],
 						"priority": ["Priority:", "5"],
@@ -201,7 +189,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task20": {
 						"context": ["Context:", "Calls" ],
 						"name": ["Task Name:", "Call City About Utility Bill"],
 						"priority": ["Priority:", "5"],
@@ -211,7 +199,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task21": {
 						"context": ["Context:", "People" ],
 						"name": ["Task Name:", "Mike is preparing operations presentation"],
 						"priority": ["Priority:", "5"],
@@ -221,7 +209,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task22": {
 						"context": ["Context:", "People" ],
 						"name": ["Task Name:", "Jane is organizing lunch party"],
 						"priority": ["Priority:", "5"],
@@ -231,7 +219,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task23": {
 						"context": ["Context:", "People" ],
 						"name": ["Task Name:", "Joanne is creating spreadsheet for monthly budget"],
 						"priority": ["Priority:", "5"],
@@ -241,7 +229,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task24": {
 						"context": ["Context:", "People" ],
 						"name": ["Task Name:", "Joe is revising business plan"],
 						"priority": ["Priority:", "5"],
@@ -251,7 +239,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task25": {
 						"context": ["Context:", "People" ],
 						"name": ["Task Name:", "Judy is revisiting our marketing plan"],
 						"priority": ["Priority:", "5"],
@@ -261,7 +249,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task26": {
 						"context": ["Context:", "Waiting" ],
 						"name": ["Task Name:", "Research into new product line"],
 						"priority": ["Priority:", "5"],
@@ -271,7 +259,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task27": {
 						"context": ["Context:", "Waiting" ],
 						"name": ["Task Name:", "Remodel Kitchen"],
 						"priority": ["Priority:", "5"],
@@ -281,7 +269,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task28": {
 						"context": ["Context:", "Waiting" ],
 						"name": ["Task Name:", "Buy new car"],
 						"priority": ["Priority:", "5"],
@@ -291,7 +279,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task29": {
 						"context": ["Context:", "Waiting" ],
 						"name": ["Task Name:", "Rewire Cat 6 cabling throughout house"],
 						"priority": ["Priority:", "5"],
@@ -301,7 +289,7 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					},
-					{
+	"task30": {
 						"context": ["Context:", "Waiting" ],
 						"name": ["Task Name:", "Revamp company web site"],
 						"priority": ["Priority:", "5"],
@@ -311,6 +299,4 @@
 						"dDate": ["Due Date:", "2011-05-16"],
 						"notes": ["Notes:", ""]
 					}
-				]
-			}//end of WAITING tasks
 	}
