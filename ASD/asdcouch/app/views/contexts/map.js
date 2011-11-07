7c296849572664721e7665ec6ac62f53ba12c9a2
Craig Booker <craig@craigbooker.com>
2011-11-07 17:25:52 -0600

    Changed for correction to a reference

--- ASD/asdcouch/app/views/contexts/map.js
+++ ASD/asdcouch/app/views/contexts/map.js
@@ -1,9 +1,8 @@
 function (doc) {
-    if (doc._id.substr(0,5) === "task:") {
-    emit(doc._id.substr(5, 9), {
-    	"contexts": doc.context,
-    	"name": doc.name,
-    	"priority": doc.priority
+    if (doc._id.substr(0,8) === "context:") {
+    emit(doc._id.substr(8), {
+    	"Context": doc.title,
+    	"Description": doc.desc
     });
   }
 };
