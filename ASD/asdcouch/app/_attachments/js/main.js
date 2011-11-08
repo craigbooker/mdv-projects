c7fb9608ebcac67d806b44696a8db78a9bccc2bd
Craig Booker <craig@craigbooker.com>
2011-11-08 00:32:50 -0600

    Cleanup

--- ASD/asdcouch/app/_attachments/js/main.js
+++ ASD/asdcouch/app/_attachments/js/main.js
@@ -12,9 +12,15 @@ $(document).ready(function() {
             	var favorite = task.value.Favorite;
             	//console.log(context);
             	//console.log(context);
-            	$('<li class="tasks" id="jsonitem_'+index+'"><a href="#">'+context+'</a></div>').appendTo('.data_load');
+            	var divCollapse = ('<div data-role="collapsible" data-collapsed="true">');
+            	//var taskHeader = ('<h3>'+name+'</h3>';)
+            	//$('<li class="tasks" id="jsonitem_'+index+'"><a href="#">'+context+'</a></div>').appendTo('.data_load');
+            	$('<div data-role="collapsible" data-collapsed="true"><h3>'+name+'</h3>'+'<data-role="fieldcontain">').appendTo('.data_load');
 			});
             $('.data_load').listview('refresh');
 		}
 	});	
 });
+
+
+
