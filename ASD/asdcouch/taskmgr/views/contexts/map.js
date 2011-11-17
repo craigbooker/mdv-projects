663494833bf15e431960d9af1aa4091cafd6e86f
Craig Booker <craig@craigbooker.com>
2011-11-17 00:28:15 -0600

    Trying to fix my project in eclipse and git.
    Signed-off-by: Craig Booker <craig@craigbooker.com>

--- ASD/asdcouch/taskmgr/views/contexts/map.js
+++ ASD/asdcouch/taskmgr/views/contexts/map.js
@@ -2,6 +2,7 @@ function (doc) {
     if (doc._id.substr(0,8) === "context:") {
     emit(doc._id.substr(8), {
     	"Context": doc.title,
+    	"Abbreviation": doc.abrev,
     	"Description": doc.desc
     });
   }
