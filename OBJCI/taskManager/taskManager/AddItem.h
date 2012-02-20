8fd5e4e8e8ba5bb86b1daa3ebe084ef5bec7161e
Craig Booker <mail@craigbooker.com>
2012-02-20 10:10:17 -0600

    Project 4 code for Obj C I
    Signed-off-by: Craig Booker <mail@craigbooker.com>

---
+++ OBJCI/taskManager/taskManager/AddItem.h
@@ -0,0 +1,14 @@
+//
+//  AddItem.h
+//  taskManager
+//
+//  Created by Craig Booker on 2/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef taskManager_AddItem_h
+#define taskManager_AddItem_h
+
+
+
+#endif
---
+++ OBJCI/wk4practice8/wk4practice8/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  wk4practice8
+//
+//  Created by Craig Booker on 2/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
